1524a978b073b33d7c8baa5571d73dc2313b60f0
Martin Look <mlook@axway.com>
2023-05-23 16:27:46 +0200

    Fix unit test

--- src/test/java/com/axway/yamles/utils/spi/impl/EnvJsonLookupProviderTest.java
+++ src/test/java/com/axway/yamles/utils/spi/impl/EnvJsonLookupProviderTest.java
@@ -1,8 +1,8 @@
 package com.axway.yamles.utils.spi.impl;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
