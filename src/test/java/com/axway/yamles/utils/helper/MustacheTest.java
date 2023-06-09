b997aa868c3a8f0c4625fe45af0946e2d2010ad3
Martin Look <mlook@axway.com>
2023-06-09 14:50:59 +0200

    Disable Java method access in templates. Also fixes CVE-2022-37767

--- src/test/java/com/axway/yamles/utils/helper/MustacheTest.java
+++ src/test/java/com/axway/yamles/utils/helper/MustacheTest.java
@@ -6,14 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 import com.axway.yamles.utils.spi.LookupProviderException;
+
+import io.pebbletemplates.pebble.error.ClassAccessException;
 import io.pebbletemplates.pebble.error.PebbleException;
 
 class MustacheTest {
 	private static final String PROP_KEY = "mustache.test";
 	private static final String PROP_VALUE = "<lookup succeded>";
-	
+
 	private static final String PROP_ML_KEY = "mustache.test.ml";
-	private static final String PROP_ML_VALUE ="\"Escaped\nMulti Line\tText\"";
+	private static final String PROP_ML_VALUE = "\"Escaped\nMulti Line\tText\"";
 
 	@Test
 	void eval() {
@@ -26,8 +28,7 @@ class MustacheTest {
 		assertEquals(PROP_VALUE, Mustache.eval("{{ _sys('" + PROP_KEY + "') }}"));
 		assertEquals(PROP_VALUE, Mustache.eval("{{ _sys(key='" + PROP_KEY + "') }}"));
 		assertEquals("Result: " + PROP_VALUE + "!", Mustache.eval("Result: {{ _sys('" + PROP_KEY + "') }}!"));
-		assertEquals("\\\"Escaped\\nMulti Line\\tText\\\"", Mustache.eval("{{ _sys('" + PROP_ML_KEY + "') }}"));
-		
+
 		assertThrows(Exception.class, () -> {
 			Mustache.eval("{{ _sys('non_existing_propery') }}");
 		});
@@ -35,15 +36,21 @@ class MustacheTest {
 		assertThrows(LookupProviderException.class, () -> {
 			Mustache.eval("{{ _sys() }}");
 		});
-		
+
 		assertThrows(PebbleException.class, () -> {
 			Mustache.eval("{{ _sys(lookup='key') }}");
 		});
-		
+
 		assertThrows(PebbleException.class, () -> {
 			Mustache.eval("{{hello}}");
 		});
 
 		System.getProperties().remove(PROP_KEY);
+	}
+
+	@Test
+	void fixed_CVE_2022_37767() throws Exception {
+		assertThrows(ClassAccessException.class,
+				() -> Mustache.eval(CVE_2022_37767_Test.TEMPLATE_INVOKE_TOSTRING_METHOD));
 	}
 }
